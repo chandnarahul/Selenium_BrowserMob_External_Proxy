a00b96a172aeedf03268b92d0f88d5883bfa2216
rahul <chandnarahul@yahoo.com>
2017-03-05 14:11:29 +0530

    refactored to use the http class

--- src/main/java/selenium/external/proxy/ProxyHandler.java
+++ src/main/java/selenium/external/proxy/ProxyHandler.java
@@ -1,25 +1,17 @@
 package selenium.external.proxy;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class ProxyHandler {
-    private static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
 
     public void startProxy() throws Exception {
-        System.out.println(sendPost(GlobalProxyConfig.proxyInit()));
+        System.out.println(ProxyHttpAPI.response(GlobalProxyConfig.proxyInit(), "POST", "port=" + GlobalProxyConfig.PROXY_RUNNING_PORT));
     }
 
     public void startHar() throws Exception {
-        System.out.println(sendPut(GlobalProxyConfig.harURL()));
+        System.out.println(ProxyHttpAPI.response(GlobalProxyConfig.harURL(), "PUT", "captureContent=true"));
     }
 
     public String getAllHAR() throws Exception {
-        return sendGet(GlobalProxyConfig.harURL());
+        return ProxyHttpAPI.response(GlobalProxyConfig.harURL(), "GET", "").data;
     }
 
     public String getSubHar(String type, String... searchParams) throws Exception {
@@ -39,61 +31,4 @@ public class ProxyHandler {
         return "";
     }
 
-    private String sendPost(String url) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("POST");
-        con.setDoOutput(true);
-        con.setRequestProperty("Content-Type", CONTENT_TYPE);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes("port=" + GlobalProxyConfig.PROXY_RUNNING_PORT);
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-        return getResponse(con).toString();
-    }
-
-    private String sendPut(String url) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("PUT");
-        con.setDoOutput(true);
-        con.setRequestProperty("Content-Type", CONTENT_TYPE);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes("captureContent=true");
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'PUT' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-        return getResponse(con).toString();
-    }
-
-    private String sendGet(String url) throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        return getResponse(con).toString();
-    }
-
-    private StringBuffer getResponse(HttpURLConnection con) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-        return response;
-    }
 }
